6c98e16babef15116203d18a85b3a6f9869296ea
iamhopaul123 <penghaoh@amazon.com>
2024-01-05 14:07:03 -0800

    First

---
+++ hello/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
+// Constants
+const PORT = 80;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/list', (req, res) => {
+    s3.listBuckets({}, (err, data) => {
+        if (err) {
+            console.log(`Caught error ${err}.`);
+            res.status(500).send('Error listing S3 buckets');
+        } else {
+            console.log('S3 Buckets:', data.Buckets);
+            res.json(data.Buckets.map(bucket => bucket.Name));
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Running on http://0.0.0.0:${PORT}`);
+});
